0abaad188d7c30f5f2b172a59c13c688aeebb2a2
quiquee <enrique.melero@gmail.com>
2020-12-28 13:11:06 +0100

    Deleted non needed files added by batu's commit. I have to tell him to do the same in his local repo

--- reader.py
+++ reader.py
@@ -1,8 +1,6 @@
 from max30100 import *
 import time
 import json
-from datetime import datetime
-#import matplotlip.pyplot as plt
 
 mx30 = MAX30100()
 
@@ -14,17 +12,13 @@ def create_values(t):
     read_values = {}
     t1 = time.time()
     count = 0
-    warmup = 100
     while (time.time()-t1) < t:
         mx30.read_sensor()
         ir = mx30.ir
         red = mx30.red
-        warmup = warmup -1 
-        if warmup <0: 
-            read_values[count] = {"ir": ir, "red": red, "timestamp": time.time()}
-            count += 1
-        
-#    print("remove finger")
+        read_values[count] = {"ir": ir, "red": red}
+        count += 1
+    print("remove finger")
     return read_values
 
 def filtered_output(x, prev_w, alpha):
@@ -33,37 +27,46 @@ def filtered_output(x, prev_w, alpha):
     return filtered_w, y
 
 def dcremoval(values, start_w, alpha):
-    filtered_values = [] 
+    filtered_values = {}
     w_r = start_w
     w_ir = start_w
     for item in values:
         temp_ir = filtered_output(values[item]["ir"], w_ir, alpha)
         temp_r = filtered_output(values[item]["red"], w_r, alpha)
-        timestamp = values[item]["timestamp"]
         w_ir,y_ir = temp_ir
         w_r,y_r = temp_r
-        if abs(y_ir) +abs(y_r) < 100:
-            filtered_values.append( {"ir": y_ir, "r": y_r, "timestamp": timestamp} )
+        filtered_values[item] = {"ir": y_ir, "r": y_r}
     return filtered_values
 
-def plot(values):
-    arr_ir = []
-    arr_r = []
-    timestamps = []
-    
-    for item in dict:
-        arr_ir.append(dict[item]["ir"])
-        arr_r.append(dict[item]["r"])
-        timestamps.append(dict[item]["timestamp"])
+def meanDiff(M, filtered_values):
+    index = 0
+    sum = 0
+    count = 0
+    average = 0
 
-    plt.plot(timestamps, arr_ir)
-    plt.plot(timestamps, arr_r)
+    sum -= filtered_values[index]["ir"]
+    filtered[index]["ir"] = M
+    sum += filtered_values[index]["ir"]
+    index += 1
+    index = index % mean_filter_size
+
+    if count < mean_filter_size:
+        count += 1
+
+    avg = sum / count
+    return avg - M
+
+def butterworthFilter(x, filtered_values):
+    pass
+
+def beat_detect()
+
+
 
 
 values = create_values(10)
 dc_result = dcremoval(values, 20000, 0.95)
-print(json.dumps(dc_result))
-
+meandiff_result = meanDiff(0, dc_result)
 
 
         
